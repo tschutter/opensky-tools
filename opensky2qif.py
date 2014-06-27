badcbf735b6bb2ddfaf4727f75dec82fbad36b0e
Tom Schutter <t.schutter@comcast.net>
2014-06-26 19:57:08 -0600

    opensky2qif.py: write payments in addition to orders

--- opensky2qif.py
+++ opensky2qif.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-"""
-Each row in a payments CSV file exported from OpenSky corresponds to a
+"""Each row in a payments CSV file exported from OpenSky corresponds to a
 single product in an order.  This script merges those rows into a
 single order.
 
@@ -10,6 +9,7 @@ gnucash-2.6.1.  It has not been tested with Quicken.
 
 QIF format: http://en.wikipedia.org/wiki/QIF
 """
+
 
 from __future__ import print_function
 import argparse
@@ -20,11 +20,17 @@ import Tkinter
 import tkFileDialog
 import tkMessageBox
 
+# Each line in an OpenSky "payment details" file corresponds to a line
+# item in a user order.  Lines with the same "Order ID" can be
+# combined into a single order.  Lines with the same "Payment date"
+# can be combined into a single payment to the vendor from OpenSky.
 
-class Order:
+
+class Order(object):
+    """A single user order."""
     def __init__(
         self,
-        date,
+        order_date,
         sku,
         item_price,
         shipping,
@@ -35,8 +41,8 @@ class Order:
         opensky_commission,
         total_payment
     ):
-        self.date = date
-        self.skus = sku
+        self.order_date = order_date
+        self.skus = [sku]
         self.item_price = item_price
         self.shipping = shipping
         self.opensky_credits = opensky_credits
@@ -58,7 +64,11 @@ class Order:
         opensky_commission,
         total_payment
     ):
-        self.skus += "," + sku
+        """Update the values with another line item row with the same
+        'Order Id'.
+        """
+        if sku not in self.skus:
+            self.skus.append(sku)
         self.item_price += item_price
         self.shipping += shipping
         self.opensky_credits += opensky_credits
@@ -68,7 +78,8 @@ class Order:
         self.opensky_commission += opensky_commission
         self.total_payment += total_payment
 
-    def validate(self, ui, order_id):
+    def validate(self, app_ui, order_id):
+        """Emit a warning if the values to not add up."""
         item_price = round(
             self.total_payment - (
                 self.shipping +
@@ -81,7 +92,7 @@ class Order:
             2
         )
         if round(self.item_price, 2) != item_price:
-            ui.warning(
+            app_ui.warning(
                 (
                     "For Order ID = {},"
                     " Item price {:.2f} != calculated price {:.2f}"
@@ -94,6 +105,21 @@ class Order:
             self.item_price = item_price
 
 
+class Payment(object):
+    """A single payment from OpenSky."""
+    def __init__(self, order_id, total_payment):
+        self.order_ids = [order_id]
+        self.total_payment = total_payment
+
+    def update(self, order_id, total_payment):
+        """Update the values with another line item row with the same
+        'Payment date'.
+        """
+        if order_id not in self.order_ids:
+            self.order_ids.append(order_id)
+        self.total_payment += total_payment
+
+
 def get_float(fields, key):
     """Convert a string value to a float, handling blank values."""
     value = fields[key]
@@ -104,14 +130,16 @@ def get_float(fields, key):
     return value
 
 
-def read_opensky(ui):
+def read_opensky(app_ui):
     """Read a .csv file exported from OpenSky."""
 
     orders = {}
+    payments = {}
     try:
-        with open(ui.args.csvfile) as opensky_file:
+        with open(app_ui.args.csvfile) as opensky_file:
             opensky_reader = csv.DictReader(opensky_file)
-            for rownum, fields in enumerate(opensky_reader):
+            for fields in opensky_reader:
+                # Get information from line item row.
                 try:
                     order_id = fields["Order ID"]
                     sku = fields["SKU"]
@@ -127,14 +155,25 @@ def read_opensky(ui):
                         "OpenSky commission"
                     )
                     total_payment = get_float(fields, "Total payment")
-                    date = fields["Original order date"]
+                    order_date = fields["Original order date"]
+                    payment_date = fields["Payment date"]
                 except KeyError as kex:
-                    ui.fatal(
+                    app_ui.fatal(
                         (
                             "Unable to find {} column in {}."
                             " Is this an OpenSky payments file?"
-                        ).format(kex, ui.args.csvfile)
+                        ).format(kex, app_ui.args.csvfile)
                     )
+
+                # Convert order date from MM/DD/YYYY to YYYY-MM-DD.
+                # This prevents GnuCash from asking you to "set a date
+                # format for this QIF file" if the dates in the file
+                # are ambiguous.
+                order_date = "{0[2]}-{0[0]}-{0[1]}".format(
+                    order_date.split("/")
+                )
+
+                # Update orders.
                 if order_id in orders:
                     orders[order_id].update(
                         sku,
@@ -149,7 +188,7 @@ def read_opensky(ui):
                     )
                 else:
                     orders[order_id] = Order(
-                        date,
+                        order_date,
                         sku,
                         item_price,
                         shipping,
@@ -160,16 +199,25 @@ def read_opensky(ui):
                         opensky_commission,
                         total_payment
                     )
+
+                # Update payments.
+                if payment_date in payments:
+                    payments[payment_date].update(order_id, total_payment)
+                else:
+                    payments[payment_date] = Payment(order_id, total_payment)
+
     except IOError:
-        ui.fatal("Unable to open {} for reading".format(ui.args.csvfile))
+        app_ui.fatal(
+            "Unable to open {} for reading".format(app_ui.args.csvfile)
+        )
 
-    return orders
+    return (orders, payments)
 
 
-def validate_orders(ui, orders):
+def validate_orders(app_ui, orders):
     """Check that the numbers add up for each order."""
     for order_id in orders:
-        orders[order_id].validate(ui, order_id)
+        orders[order_id].validate(app_ui, order_id)
 
 
 def write_split(qif_file, amount, account, memo=None):
@@ -181,64 +229,116 @@ def write_split(qif_file, amount, account, memo=None):
         print("${:.2f}".format(amount), file=qif_file)
 
 
-def write_qif(ui, orders):
+def write_qif_header(app_ui, qif_file):
+    """Write header to a .qif file."""
+    print("!Account", file=qif_file)
+    print("N{}".format(app_ui.args.acct_opensky), file=qif_file)
+    print("TBank", file=qif_file)
+    print("^", file=qif_file)
+    print("!Type:Bank", file=qif_file)
+
+
+def list_to_string(item_name, values):
+    """Return a pretty version of the list."""
+    values = sorted(values)
+    if len(values) < 5:
+        values_string = "{}{}={}".format(
+            item_name,
+            "s" if len(values) > 1 else "",
+            ",".join(values)
+        )
+    else:
+        values_string = "{} {}s from {} to {}".format(
+            len(values),
+            item_name,
+            values[0],
+            values[-1]
+        )
+    return values_string
+
+
+def write_qif_orders(app_ui, orders, qif_file):
+    """Write orders to a .qif file."""
+    for order_id in sorted(orders):
+        order = orders[order_id]
+        print("D{}".format(order.order_date), file=qif_file)
+        print("POpenSky order {}".format(order_id), file=qif_file)
+        print("L{}".format(app_ui.args.acct_opensky), file=qif_file)
+        print("T{:.2f}".format(order.total_payment), file=qif_file)
+        # It appears that splits show up in GNUCash in the
+        # opposite order than they are specified in the
+        # imported file.
+        write_split(
+            qif_file,
+            order.opensky_commission,
+            app_ui.args.acct_commission
+        )
+        write_split(
+            qif_file,
+            order.cc_processing,
+            app_ui.args.acct_cc_processing
+        )
+        write_split(
+            qif_file,
+            order.restocking_fee,
+            app_ui.args.acct_restocking
+        )
+        write_split(
+            qif_file,
+            order.sales_tax,
+            app_ui.args.acct_sales_tax
+        )
+        write_split(
+            qif_file,
+            order.opensky_credits,
+            app_ui.args.acct_credits
+        )
+        write_split(
+            qif_file,
+            order.shipping,
+            app_ui.args.acct_shipping
+        )
+        write_split(
+            qif_file,
+            order.item_price,
+            app_ui.args.acct_sales,
+            list_to_string("SKU", order.skus)
+        )
+        print("^", file=qif_file)
+
+
+def write_qif_payments(app_ui, payments, qif_file):
+    """Write payments to a .qif file."""
+    for payment_date in sorted(payments):
+        payment = payments[payment_date]
+        print("D{}".format(payment_date), file=qif_file)
+        print("POpenSky payment", file=qif_file)
+        print("L{}".format(app_ui.args.acct_opensky), file=qif_file)
+        print("T{:.2f}".format(-payment.total_payment), file=qif_file)
+        write_split(
+            qif_file,
+            payment.total_payment,
+            app_ui.args.acct_deposit,
+            "{}".format(list_to_string("Order ID", payment.order_ids)),
+        )
+        print("^", file=qif_file)
+
+
+def write_qif(app_ui, orders, payments):
     """Write a .qif file."""
     try:
-        with open(ui.args.qiffile, "w") as qif_file:
-            print("!Account", file=qif_file)
-            print("N{}".format(ui.args.acct_opensky), file=qif_file)
-            print("TBank", file=qif_file)
-            print("^", file=qif_file)
-            print("!Type:Bank", file=qif_file)
-            for order_id in sorted(orders):
-                order = orders[order_id]
-                print("D{}".format(order.date), file=qif_file)
-                print("POpenSky order {}".format(order_id), file=qif_file)
-                print("L{}".format(ui.args.acct_opensky), file=qif_file)
-                print("T{:.2f}".format(order.total_payment), file=qif_file)
-                # It appears that splits show up in GNUCash in opposite order.
-                write_split(
-                    qif_file,
-                    order.opensky_commission,
-                    ui.args.acct_commission
-                )
-                write_split(
-                    qif_file,
-                    order.cc_processing,
-                    ui.args.acct_cc_processing
-                )
-                write_split(
-                    qif_file,
-                    order.restocking_fee,
-                    ui.args.acct_restocking
-                )
-                write_split(
-                    qif_file,
-                    order.sales_tax,
-                    ui.args.acct_sales_tax
-                )
-                write_split(
-                    qif_file,
-                    order.opensky_credits,
-                    ui.args.acct_credits
-                )
-                write_split(
-                    qif_file,
-                    order.shipping,
-                    ui.args.acct_shipping
-                )
-                write_split(
-                    qif_file,
-                    order.item_price,
-                    ui.args.acct_sales,
-                    order.skus
-                )
-                print("^", file=qif_file)
+        with open(app_ui.args.qiffile, "w") as qif_file:
+            write_qif_header(app_ui, qif_file)
+            write_qif_orders(app_ui, orders, qif_file)
+            write_qif_payments(app_ui, payments, qif_file)
     except IOError:
-        ui.fatal("Unable to open {} for writing".format(ui.args.qiffile))
+        app_ui.fatal(
+            "Unable to open {} for writing".format(app_ui.args.qiffile)
+        )
 
 
 class AppUI(object):
+    """Base application user interface."""
     def __init__(self, args):
         self.args = args
 
@@ -246,16 +346,18 @@ class AppUI(object):
         """Perform the actual conversion."""
 
         # Read a .csv file exported from OpenSky.
-        orders = read_opensky(self)
+        orders, payments = read_opensky(self)
 
         # Check that the numbers add up for each order.
         validate_orders(self, orders)
 
         # Write a .qif file.
-        write_qif(self, orders)
+        write_qif(self, orders, payments)
 
 
 class AppGUI(AppUI):
+    """Application graphical user interface."""
+    # pylint: disable=no-self-use
     def __init__(self, args, root):
         AppUI.__init__(self, args)
         self.root = root
@@ -317,19 +419,22 @@ class AppGUI(AppUI):
         )
         self.cancel_button.pack(side=Tkinter.LEFT, padx=5, pady=5)
 
+    def set_csvfile(self, csvfile):
+        """Set the name of the CSV file."""
+        self.args.csvfile = csvfile
+        self.csv_entry.delete(0, Tkinter.END)
+        self.csv_entry.insert(0, csvfile)
+        root = os.path.splitext(csvfile)[0]
+        self.set_qiffile(root + ".qif")
+
     def set_qiffile(self, qiffile):
+        """Set the name of the QIF file."""
         self.args.qiffile = qiffile
         self.qif_entry.delete(0, Tkinter.END)
         self.qif_entry.insert(0, qiffile)
 
-    def set_csvfile(self, csvfile):
-        self.args.csvfile = csvfile
-        self.csv_entry.delete(0, Tkinter.END)
-        self.csv_entry.insert(0, csvfile)
-        root, ext = os.path.splitext(csvfile)
-        self.set_qiffile(root + ".qif")
-
     def select_csvfile(self):
+        """Display a dialog to select a CSV file."""
         self.args.csvfile = self.csv_entry.get()
         csvfile = tkFileDialog.askopenfilename(
             title="Choose .csv file",
@@ -342,6 +447,7 @@ class AppGUI(AppUI):
             self.set_csvfile(csvfile)
 
     def select_qiffile(self):
+        """Display a dialog to select a QIF file."""
         self.args.qiffile = self.qif_entry.get()
         qiffile = tkFileDialog.asksaveasfilename(
             parent=self.root,
@@ -354,17 +460,21 @@ class AppGUI(AppUI):
             self.set_qiffile(qiffile)
 
     def error(self, msg):
+        """Display an error message."""
         tkMessageBox.showerror("Error", msg)
 
     def fatal(self, msg):
+        """Display an error message and exit."""
         self.error(msg)
         self.frame.quit()
         sys.exit(1)
 
     def warning(self, msg):
+        """Display a warning message."""
         tkMessageBox.showwarning("Warning", msg)
 
     def convert_action(self):
+        """Perform the conversion and exit."""
         self.args.csvfile = self.csv_entry.get()
         self.args.qiffile = self.qif_entry.get()
         if not self.args.csvfile:
@@ -377,17 +487,22 @@ class AppGUI(AppUI):
 
 
 class AppCLI(AppUI):
+    """Application command line user interface."""
+    # pylint: disable=no-self-use
     def __init__(self, args):
         AppUI.__init__(self, args)
 
     def error(self, msg):
+        """Display an error message."""
         print("ERROR: {}".format(msg), file=sys.stderr)
 
     def fatal(self, msg):
+        """Display an error message and exit."""
         self.error(msg)
         sys.exit(1)
 
     def warning(self, msg):
+        """Display a warning message."""
         print("WARNING: {}".format(msg), file=sys.stderr)
 
 
@@ -423,6 +538,12 @@ def main():
         metavar="NAME",
         default="Expenses:OpenSky Commissions",
         help="OpenSky commissions expense account (default=%(default)s)"
+    )
+    arg_parser.add_argument(
+        "--acct-deposit",
+        metavar="NAME",
+        default="Assets:Checking",
+        help="payment deposit account (default=%(default)s)"
     )
     arg_parser.add_argument(
         "--acct-opensky",
@@ -472,7 +593,7 @@ def main():
             if args.csvfile == args.qiffile:
                 arg_parser.error("csvfile and qiffile are the same file")
         else:
-            (root, ext) = os.path.splitext(args.csvfile)
+            root = os.path.splitext(args.csvfile)[0]
             args.qiffile = root + ".qif"
     else:
         if not args.gui:
